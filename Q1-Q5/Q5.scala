0c45d26136efdc9f6bd462ad6f95eaf8d1f83ee7
Pasindu <pasindugura123@gmail.com>
2022-07-18 11:03:38 +0530

    I added the answers

--- Q1-Q5/Q5.scala
+++ Q1-Q5/Q5.scala
@@ -1,12 +1,12 @@
 object Q5{
     def main(args: Array[String]): Unit ={
-        val time:Double=easy_pace(2)+easy_pace(2)+tempo(3)
+        val time:Int=easy_pace(2)+easy_pace(2)+tempo(3)
         println("The total running time:"+time+" min")
     }
-    def easy_pace(x:Int):Double={
-        return x/8.0
+    def easy_pace(x:Int):Int={
+        return x*8
     }
-    def tempo(y:Int):Double={
-        return y/7.0
+    def tempo(y:Int):Int={
+        return y*7
     }
 }
